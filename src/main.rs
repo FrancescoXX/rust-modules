6c13cf16140046b1488f9ef6000b110da43dfa03
FrancescoXX <francescociullaroma@gmail.com>
2024-04-29 15:11:42 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden;
+
+fn main() {
+    let plant: Asparagus = Asparagus {
+        name: String::from("Asparagus"),
+        stalks: 5,
+    };
+
+    println!("I'm growing {:?}!", plant);
+}
+
